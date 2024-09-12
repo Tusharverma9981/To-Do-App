41ac8e611519f7fa3b7e30103026a99b61c471c9
Tushar <tv4648551@gmail.com>
2024-09-12 22:45:58 +0530

    completed

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let todolist = [];
+displayItem()
 
 function addtodo(){
     let inputitem = document.querySelector('#input_element');
     let todoitem = inputitem.value;
     todolist.push(todoitem);
     inputitem.value = '';
+    displayItem()
+}
+function displayItem(){
+    let displayElement = document.querySelector('#todo-items')
+    let newhtml = '';
+    for (let index = 0; index < todolist.length; index++) {
+       newhtml += `
+       <div>
+       <span>${todolist[index]}<span>
+       <button onclick=" todolist.splice(${index},1); displayItem() ">Delete</button>
+       </div>
+       `
+        
+    }
+    displayElement.innerHTML = newhtml;
 }
